82318d66ece88c74831e0d92781fbd8b8e45c8e2
wilfredcho <wilfredcho@gmail.com>
2020-12-24 12:12:31 -0800

    fix param arg (#18)

--- tdameritrade/client.py
+++ tdameritrade/client.py
@@ -264,7 +264,7 @@ class TDClient(object):
 
         params['needExtendedHoursData'] = needExtendedHoursData
 
-        return self._request(GET_PRICE_HISTORY.format(symbol=symbol), data=params)
+        return self._request(GET_PRICE_HISTORY.format(symbol=symbol), params=params)
 
     def historyDF(self, symbol, **kwargs):
         '''get history as dataframe'''
